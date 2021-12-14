3a0abd2cb319a15ebe7e571560d2c1a09e7335ca
ggardusi <gustavo.gardusi@gmail.com>
2021-12-14 00:03:04 -0300

    remove unused imports

--- string_autocomplete/java/src/test/java/com/solution/trie/TrieTests.java
+++ string_autocomplete/java/src/test/java/com/solution/trie/TrieTests.java
@@ -1,12 +1,10 @@
 package com.solution.trie;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
